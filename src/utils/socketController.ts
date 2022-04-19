597375a961300eec6ba18985810112af62aa46f1
AnjiBoss <nhu.nhu.face@gmail.com>
2022-04-19 16:49:34 +0900

    rooms

--- src/utils/socketController.ts
+++ src/utils/socketController.ts
@@ -1,38 +1,82 @@
 import { Server, Socket } from "socket.io";
 
+interface Room {
+  roomName: string;
+  users: string[];
+}
+
+const rooms: Room[] = [];
+
 const socketController = (socket: Socket, _io: Server) => {
-  socket.on("calling", () => {
-    socket.broadcast.emit("incoming-call");
+  socket.on("room-list", () => {
+    socket.emit("rooms", { rooms });
   });
 
-  socket.on("accept", () => {
+  socket.on("create-room", ({ roomName, user }) => {
+    rooms.push({ roomName, users: [user] });
+    socket.join(roomName);
+    socket.broadcast.emit("new-room", { roomName, users: [user] });
+    socket.emit("notify", { message: "new room created" });
+  });
+
+  socket.on("leave-room", ({ roomName, user }) => {
+    console.log("leave-room", { roomName, user });
+    rooms.forEach((r, i) => {
+      if (r.roomName === roomName) {
+        rooms[i].users = rooms[i].users.filter((u) => u !== user);
+      }
+    });
+    socket.to(roomName).emit("user-leave-room", { user: user });
+  });
+
+  socket.on("join-room", ({ roomName, user }) => {
+    socket.join(roomName);
+    rooms.forEach((r, i) => {
+      if (r.roomName === roomName) {
+        rooms[i].users.push(user);
+      }
+    });
+    socket.to(roomName).emit("user-join-room", { user: user });
+    socket.emit("notify", { message: "Joined room" });
+  });
+
+  socket.on("calling", ({ roomName }) => {
+    socket.to(roomName).emit("incoming-call");
+  });
+
+  socket.on("accept", ({ roomName }) => {
     console.log("call-accepted", socket.id);
-    socket.broadcast.emit("accepted");
+    socket.to(roomName).emit("accepted");
   });
 
-  socket.on("deny", () => {
+  socket.on("deny", ({ roomName }) => {
     console.log("denied");
-    socket.broadcast.emit("denied");
+    socket.to(roomName).emit("denied");
   });
 
-  socket.on("send-offer", ({ offer }) => {
+  socket.on("send-offer", ({ offer, roomName }) => {
     console.log("send-offer", socket.id);
-    socket.broadcast.emit("get-offer", { offer });
+    socket.to(roomName).emit("get-offer", { offer });
   });
 
-  socket.on("send-answer", ({ answer }) => {
+  socket.on("send-answer", ({ answer, roomName }) => {
     console.log("send-answer");
-    socket.broadcast.emit("answered", { answer });
+    socket.to(roomName).emit("answered", { answer });
   });
 
-  socket.on("offer-send-candidate", ({ candidate }) => {
+  socket.on("offer-send-candidate", ({ candidate, roomName }) => {
     console.log("offer send candidate");
-    socket.broadcast.emit("add-offer-candidate", { offer: candidate });
+    socket.to(roomName).emit("add-offer-candidate", { offer: candidate });
   });
 
-  socket.on("answer-send-candidate", ({ candidate }) => {
+  socket.on("answer-send-candidate", ({ candidate, roomName }) => {
     console.log("answer-send-candidate");
-    socket.broadcast.emit("add-answer-candidate", { answer: candidate });
+    socket.to(roomName).emit("add-answer-candidate", { answer: candidate });
   });
 };
+
+setInterval(() => {
+  console.log(rooms);
+}, 3000);
+
 export default socketController;
